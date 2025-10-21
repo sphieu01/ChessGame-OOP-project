b6a805dc0eda89e35afb30f4d18ed4e97b3b8d06
Dao Hoang <Daodanghoang2005@gmail.com>
2025-10-21 23:08:30 +0700

    Nhập Thành

--- ChessBTL/src/piece/King.java
+++ ChessBTL/src/piece/King.java
@@ -26,6 +26,34 @@ public class King extends Piece {
                     return true;
                 }
             }
+            if(moved == false){
+                //Right castling
+                if(targetCol == preCol+2 && targetRow == preRow && pieceIsOnStraightLine(targetCol,targetRow) == false){
+                    //Xét xem có con nào ở vị tri thứ 3 bên phải không
+                    for (Piece piece:GamePanel.simPieces){
+                        if(piece.col == preCol + 3 && piece.row == preRow && piece.moved == false){
+                            GamePanel.castlingP = piece;
+                            return true;
+                        }
+                    }
+                }
+                //Left castling
+                if(targetCol == preCol - 2 && targetRow == preRow && pieceIsOnStraightLine(targetCol,targetRow) == false){
+                    Piece p[] = new Piece[2];
+                    for(Piece piece:GamePanel.simPieces){
+                        if(piece.col == preCol - 3 && piece.row == targetRow){
+                            p[0] = piece;
+                        }
+                        if(piece.col == preCol-4 && piece.row == targetRow){
+                            p[1] = piece;
+                        }
+                        if(p[0] == null && p[1]!=null &&p[1].moved == false){
+                            GamePanel.castlingP = p[1];
+                            return true;
+                        }
+                    }
+                }
+            }
         }
         return false;
     }
