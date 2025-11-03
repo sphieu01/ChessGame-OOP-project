778b450e527db4ad09e9bf859c8c18ee79dfc4f8
Dao Hoang <Daodanghoang2005@gmail.com>
2025-11-03 21:29:04 +0700

    Hint

--- ChessBTL/src/main/MoveHighlighter.java
+++ ChessBTL/src/main/MoveHighlighter.java
@@ -30,4 +30,11 @@ public class MoveHighlighter {
             g2.fillRect(p.x * board.SQUARE_SIZE, p.y * board.SQUARE_SIZE, board.SQUARE_SIZE, board.SQUARE_SIZE);
         }
     }
+    public void drawhint(Graphics2D g2) {
+        // ví dụ vàng nhạt, alpha vừa phải
+        g2.setColor(new Color(255, 165, 0, 150)); 
+        for (Point p : GamePanel.hintList) {
+            g2.fillRect(p.x * board.SQUARE_SIZE, p.y * board.SQUARE_SIZE, board.SQUARE_SIZE, board.SQUARE_SIZE);
+        }
+    }
 }
